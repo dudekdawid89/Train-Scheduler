c63d269b9abaa788f2f3d12c83b3e75acb705cd3
dudekdawid89 <dudekdawid89@gmail.com>
2019-06-09 14:38:55 -0500

    / 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry

--- train.js
+++ train.js
@@ -15,11 +15,45 @@
 
 
   $("#submit").on("click", function(event){
-      event.preventDefault();
+    event.preventDefault();
 
-      var trainName = $("")
-      var trainDestination = $("")
-      var firstTrainTime = $("")
-      var trainFrequecny = $("")
+      var trainName = $("#trainName-input").val().trim();
+      var trainDestination = $("#destination-input").val().trim();
+      var firstTrainTime = $("#firstTime-input").val().trim();
+      var trainFrequecny = $("#frequecny-input").val().trim();
 
-  })
+      var newTrain = {
+        name: trainName,
+        destination: trainDestination,
+        time: firstTrainTime,
+        frequecny: trainFrequecny
+      };
+
+      database.ref().push(newTrain);
+
+      console.log(newTrain.name);
+      console.log(newTrain.destination);
+      console.log(newTrain.time);
+      console.log(newTrain.frequecny);
+
+
+      $("#trainName-input").val("");
+      $("#destination-input").val("");
+      $("#firstTime-input").val("");
+      $("#frequecny-input").val("");
+  });
+
+  database.ref().on("child_added",function(childSnapshot){
+console.log(childSnapshot.val());
+
+      var trainName = childSnapshot.val().name;
+      var trainDestination = childSnapshot.val().destination;
+      var firstTrainTime = childSnapshot.val().time;
+      var trainFrequecny = childSnapshot.val().frequecny;
+
+      console.log(trainName);
+      console.log(trainDestination);
+      console.log(firstTrainTime);
+      console.log(trainFrequecny);
+
+    });
